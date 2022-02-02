0198b5a84643462ffa08761facbe65baf67bbbd7
Argana Phangquestian <arganaphangquestian@gmail.com>
2022-02-02 16:36:31 +0700

    done

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from database import engine
 
 app = FastAPI()
 
 
 @app.get("/")
 def index():
-    return {"message": "OK"}
+    return {"message": "OK", "engine": engine.table_names}
